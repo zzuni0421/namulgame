0b3c8732118d6dc98d69b004c3ddccd5342d5ebe
zzuni0421 <jmryu615@gmail.com>
2025-08-01 20:55:08 +0900

    중복 선언

--- game/js/game1.js
+++ game/js/game1.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("gameCanvas");
   const ctx = canvas.getContext("2d");
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
