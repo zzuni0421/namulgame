44c76705e6bfd9e1ba3a45b9e1eaa3145dcba1b5
zzuni0421 <jmryu615@gmail.com>
2025-08-02 09:32:01 +0900

    Create firebase-config.js

---
+++ game/js/firebase-config.js
@@ -0,0 +1,18 @@
+// scripts/firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYd4Q3koWeOOmj6KakUZF4H0f_1dsoWBQ",
+  authDomain: "jumpgame-f54ea.firebaseapp.com",
+  projectId: "jumpgame-f54ea",
+  storageBucket: "jumpgame-f54ea.firebasestorage.app",
+  messagingSenderId: "51354244531",
+  appId: "1:51354244531:web:a6d6a117f7415defcfe7c6",
+  measurementId: "G-VELYVP3QVK"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs, query, orderBy, limit };
