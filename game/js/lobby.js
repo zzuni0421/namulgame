3bcc3699f0359fa4e3ef5e2a7b7609274d4682ad
zzuni0421 <jmryu615@gmail.com>
2025-08-24 16:12:40 +0900

    Update lobby.js

--- game/js/lobby.js
+++ game/js/lobby.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxQ3NtGJMFzIXSror3yPI8dckkWZt33U-hY-kHad4_R5DPFSR8Zz9Qold347ajx11-Juw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxRBhNINhWqqA5JhVvdhQ_YDpWWqubA5q6zsb5b7lWwqrRsAhC2ib_txdzvhvU41CdaWg/exec";
 
 const authModal = document.getElementById("authModal");
 const registerModal = document.getElementById("registerModal");
