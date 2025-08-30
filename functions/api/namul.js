a1819d7da87d6b973e762f16dd36103cbdcdde7c
zzuni0421 <jmryu615@gmail.com>
2025-08-30 22:03:58 +0900

    Update namul.js

--- functions/api/namul.js
+++ functions/api/namul.js
@@ -3,7 +3,7 @@ export async function onRequestPost({ request }) {
     const body = await request.json();
 
     const GAS_URL =
-      "https://script.google.com/macros/s/AKfycbyVzMNWFVBu-FtHS_yZ_DWL_jljLgTAdBMD5Qs3-KbVz5kPmuufiKNzQBqK2hZybCSm/exec";
+      "https://script.google.com/macros/s/AKfycbxwM0NfDkxko-8QeRF_oW7P8Pkc9IN7EqUCsorNxcCQd8hS2Z4uL6hWzWZIGCg58HADCQ/exec";
 
     // body 전체를 그대로 GAS에 전달 (action 포함)
     const res = await fetch(GAS_URL, {
